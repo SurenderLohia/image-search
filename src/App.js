dd603d0f503696f7d10f8d1643789eaaf896d2d0
Surender Lohia <surender.lohia.e@gmail.com>
2020-05-13 11:22:48 +0530

    Fetch functionality has been implemented

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+
 import './App.css';
 
 function App() {
+  const fetchImages = async (category) => {
+    const query = `?qt=${category}`
+    const url = `https://www.alamy.com/xml-search-results.asp${query}`;
+    const images = await axios.get(url);
+    console.log(images);
+  }
+  fetchImages('car');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrap">
+      <h2>Image Search</h2>
+      <div className="image-search-container">
+        <input className="input-text input-text-search" type="text" defaultValue="Search" />
+        <button className="button" type="submit">Search Image</button>
+      </div>
     </div>
   );
 }
